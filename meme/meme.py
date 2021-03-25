85db2f40b67020064f393934a6c597c6a56bde00
aaronplayz-sys <34110509+aaronplayz-sys@users.noreply.github.com>
2021-03-25 12:41:07 -0400

    Uploaded meme (ALPHA)

---
+++ meme/meme.py
@@ -0,0 +1,44 @@
+import discord
+from redbot.core import commands
+import asyncpraw
+import random
+
+reddit = asyncpraw.Reddit(
+    user_agent="RoboCop-8070",
+    client_id="7FoQYOQIEsx6DA",
+    client_secret="kGtpyGAyuZ8Zp_aBxbj3I3wGGj8SZA",
+    username="RoboCop-8070",
+    password="qVxjMuUnZigh35s",
+)
+
+class Meme(commands.Cog):
+    @commands.guild_only()
+    @commands.command(name='meme')
+    async def meme(self, ctx):
+        """
+        Generates a meme from Reddit
+        """
+        subreddit = await reddit.subreddit("dankmemes", fetch=True)
+        all_subs = []
+
+        top = subreddit.top(limit = 50)
+
+        async for submission in top:
+            all_subs.append(submission)
+        
+        random_sub = random.choice(all_subs)
+
+        name = random_sub.title
+        url = random_sub.url
+
+        embed = discord.Embed(title = name)
+
+        embed.set_image(url=url)
+
+        await ctx.send(embed=embed)
+
+        
+
+
+#def setup(bot):
+    #bot.add_cog(meme(bot))
