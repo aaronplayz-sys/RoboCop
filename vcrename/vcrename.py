ab84057b0229eb86ca5f7fb0a444fde4d786199e
aaronplayz-sys <34110509+aaronplayz-sys@users.noreply.github.com>
2024-06-10 11:36:13 -0400

    Remove local error handler
    causes more errors than handling  the current error

--- vcrename/vcrename.py
+++ vcrename/vcrename.py
@@ -16,23 +16,3 @@ class vcrename(commands.Cog):
     embed.add_field(name='Sucess!', value='The voice channel has been updated!')
 
     await ctx.send(embed=embed)
-
-  @vcrename.error
-  async def vcrename_error(self, ctx: commands.Context, error):
-    if isinstance(error, commands.MissingPermissions):
-      embed = discord.Embed(
-        color=discord.Color.Dark_red()
-      )
-
-      embed.add_field(name='Sorry, looks like you do not have the right permissions.', value='Do you have the manage channel permission?')
-
-      await ctx.send(embed=embed)
-    
-    if isinstance(error, commands.MissingRequiredArgument):
-      embed = discord.embed(
-        color=discord.Color.gold()
-      )
-
-      embed.add_field(name='Looks like you forgot something...', value='Syntax: [Prefix]vcrename "VoiceChanelName" NewVoiceChannelName')
-
-      await ctx.send(embed=embed)
